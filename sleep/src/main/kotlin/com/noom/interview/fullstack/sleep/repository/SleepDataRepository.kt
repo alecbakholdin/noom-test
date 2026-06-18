ef37b96331180cf4b8841acebfd78400b4e0d2ab
Alec Bakholdin <alecbakholdin@gmail.com>
2026-06-17 22:43:08 -0400

    feat: added method to get last sleep log to repo

--- sleep/src/main/kotlin/com/noom/interview/fullstack/sleep/repository/SleepDataRepository.kt
+++ sleep/src/main/kotlin/com/noom/interview/fullstack/sleep/repository/SleepDataRepository.kt
@@ -1,7 +1,9 @@
 package com.noom.interview.fullstack.sleep.repository
 
 import com.noom.interview.fullstack.sleep.model.SleepData
+import com.noom.interview.fullstack.sleep.model.SleepQuality
 import org.springframework.stereotype.Repository
+import java.sql.ResultSet
 
 @Repository
 class SleepDataRepository(private val db: DatabaseInterface) {
@@ -21,5 +23,32 @@ class SleepDataRepository(private val db: DatabaseInterface) {
         if (modifiedCount == 0) {
             throw IllegalStateException("Failed to create sleep data")
         }
+    }
+
+    fun getLastSleepData(userId: Int): SleepData? {
+        val query = """SELECT 
+            |  id,
+            |  user_id,
+            |  date,
+            |  time_start,
+            |  duration_hours,
+            |  quality
+            |FROM sleep_data 
+            |WHERE user_id = ?
+            |ORDER BY date DESC, time_start DESC
+            |LIMIT 1
+            |""".trimMargin()
+        return db.findOne(::deserializeSleepData, query, userId)
+    }
+
+    fun deserializeSleepData(resultSet: ResultSet): SleepData {
+        return SleepData(
+            id = resultSet.getInt("id"),
+            userId = resultSet.getInt("user_id"),
+            date = resultSet.getDate("date"),
+            timeStart = resultSet.getTime("time_start"),
+            durationHours = resultSet.getInt("duration_hours"),
+            quality = SleepQuality.valueOf(resultSet.getString("quality")),
+        )
     }
 }
